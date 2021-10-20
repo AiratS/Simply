e36c4c2190b50e84d808e6d7c3bbdbd5aa911479
Airat Sirazov <sirazovairat@yandex.ru>
2021-10-20 16:42:08 +0300

    feat: create the project base

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index(): void
+    {
+        dd('DefaultController::index');
+    }
+}
+
