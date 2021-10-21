374336d23b069a2c86de52b1d102be78510dc1cf
Airat Sirazov <sirazovairat@yandex.ru>
2021-10-21 16:48:45 +0300

    refactor: add return type App\Controller\Public\AppController::index

--- src/Controller/Public/AppController.php
+++ src/Controller/Public/AppController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Public;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
@@ -12,7 +13,7 @@ class AppController extends AbstractController
     /**
      * @Route("/{publicPath}", name="public_path", defaults={"publicPath": null})
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('public/index.html.twig');
     }
