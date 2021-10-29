f2a2a9aec5ef71f2203010383a0b031443c27947
Airat Sirazov <sirazovairat@yandex.ru>
2021-10-29 17:10:45 +0300

    feat: create Profile page view

---
+++ assets/js/components/ProfileAvatar.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="profile-avatar">
+    <div class="profile-avatar-picture">
+      <div class="profile-avatar-picture-frame">
+        <img src="../../img/profile-avatar.png" alt="profile-avatar" />
+      </div>
+      <b-button class="profile-avatar-change sy-btn">
+        <b-icon icon="camera-fill"></b-icon>
+      </b-button>
+    </div>
+    <div class="profile-avatar-friends">
+      <p class="profile-avatar-friends-title">Friends</p>
+      <p class="profile-avatar-friends-count">
+        {{ friendsCount }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  BButton
+} from 'bootstrap-vue';
+
+export default {
+  name: "ProfileAvatar",
+  components: {
+    BButton,
+  },
+  props: {
+    avatar: {
+      type: String,
+      required: true,
+    },
+    friendsCount: {
+      type: Number,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../scss/colors';
+
+.profile-avatar {
+  .profile-avatar-friends {
+    text-align: center;
+    margin-top: 28px;
+
+    .profile-avatar-friends-title {
+      color: #686868;
+      font-size: 20px;
+      font-weight: 500;
+      margin-bottom: 6px;
+    }
+
+    .profile-avatar-friends-count {
+      color: #000000;
+      font-size: 20px;
+      font-weight: 600;
+    }
+  }
+}
+
+.profile-avatar-picture {
+  position: relative;
+  text-align: center;
+
+  .profile-avatar-picture-frame {
+    padding: 4px;
+    width: 178px;
+    height: 178px;
+    border-radius: 89px;
+    background-color: white;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .profile-avatar-change {
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    color: white;
+    top: -8px;
+    right: 49px;
+    padding-left: 10px;
+
+    &:hover {
+      border: 2px solid white;
+      padding-left: 8px;
+    }
+  }
+}
+</style>
